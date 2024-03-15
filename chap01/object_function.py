aca659787cc89a7e7fffc9db1d8836be036f8092
sh940701 <visioner2168@gmail.com>
2024-03-15 16:00:16 +0900

    object_function.py

---
+++ chap01/object_function.py
@@ -0,0 +1,22 @@
+# n = 1
+#
+#
+# def put_x():
+#     x = 1
+#     print(f'id(x) = {id(x)}')
+#
+#
+# print(f'id(1) = {id(1)}')
+# print(f'id(n) = {id(n)}')
+# put_x()
+
+def put_y():
+    y = 2
+    print(f'id(y) = {id(y)}')
+
+
+put_y()
+
+z = 2
+print(f'id(2) = {id(2)}')
+print(f'id(z) = {id(z)}')
