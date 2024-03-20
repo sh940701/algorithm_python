dafe3a21f07060dea8423928603955aa5080cfd5
sh940701 <visioner2168@gmail.com>
2024-03-20 23:55:20 +0900

    14501.py

---
+++ BaekJoon/S/14501.py
@@ -0,0 +1,27 @@
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+result = float('-inf')
+
+
+def recur(idx, benefit):
+    global result
+
+    if idx == N:
+        result = max(result, benefit)
+        return
+    # 퇴사일 이후에는 일을 할 수 없음
+    if idx > N:
+        return
+
+    # 상담을 할 때
+    # 다른 상담을 하고 있는 중에는 상담을 할 수 없음
+    recur(idx + arr[idx][0], benefit + arr[idx][1])
+
+    # 상담을 하지 않을 때
+    recur(idx + 1, benefit)
+
+
+recur(0, 0)
+
+print(result)
