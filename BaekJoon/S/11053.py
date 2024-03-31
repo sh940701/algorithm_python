4b1ef8556c06002e3f3250bb3206e9674dabe8fb
sh940701 <visioner2168@gmail.com>
2024-04-01 01:14:56 +0900

    11053.py

--- BaekJoon/S/11053.py
+++ BaekJoon/S/11053.py
@@ -1,16 +1,10 @@
 N = int(input())
-arr = [0] + list(map(int, input().split()))
+arr = list(map(int, input().split()))
+dp = [1] * N
 
-result = float('-inf')
+for i in range(1, N):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
-for i in range(N):
-    count = 1
-    max_value = arr[i]
-    for j in range(i, N):
-        if max_value < arr[j]:
-            max_value = arr[j]
-            count += 1
-
-    result = max(result, count)
-
-print(result)
+print(max(dp))
